3aee1e5279ba1ac1af83ba96f16addb79206caaf
ShubhamGhumare11 <shubhamghumare@gtasterix.com>
2024-10-18 11:25:16 +0530

    changes in CORS configuration code

--- src/main/java/com/spring/jwt/config/AppConfig.java
+++ src/main/java/com/spring/jwt/config/AppConfig.java
@@ -98,6 +98,7 @@ public class AppConfig {
                 ).permitAll()
                 .requestMatchers("/account/**").permitAll()
                 .requestMatchers("/api/otp/**").permitAll()
+                .requestMatchers("/user/form/**").permitAll()
 
 
                 .anyRequest().authenticated()
@@ -123,7 +124,7 @@ public class AppConfig {
             @Override
             public CorsConfiguration getCorsConfiguration(HttpServletRequest request) {
                 CorsConfiguration config = new CorsConfiguration();
-                config.setAllowedOrigins(Collections.singletonList("http://localhost:5173"));
+                config.setAllowedOrigins(Collections.singletonList("http://localhost:5173,http://localhost:3000"    ));
                 config.setAllowedMethods(Collections.singletonList("*"));
                 config.setAllowCredentials(true);
                 config.setAllowedHeaders(Collections.singletonList("*"));
--- src/main/java/com/spring/jwt/controller/AdminController.java
+++ src/main/java/com/spring/jwt/controller/AdminController.java
@@ -1,12 +1,13 @@
 package com.spring.jwt.controller;
 
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.security.Principal;
-
+@CrossOrigin(origins = "http://localhost:3000")
 @RestController
 @RequestMapping("/admin")
 public class AdminController {
